599e79e195f5cd126562b37c3acb566de8ceb471
Your Name <you@example.com>
2025-02-18 09:41:24 +0530

    add mes

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const server=http.createServer((req,res)=>{
     res.write("hello world")
     res.end("response provide")
 });
-server.listen("3201","192.168.1.52",()=>{console.log("server running")})
+server.listen("3201",()=>{console.log("server running")})
